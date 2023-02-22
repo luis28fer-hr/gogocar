d3b15579546b4468be63e17e50d928d8fb6ab9e4
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2023-02-22 15:11:01 -0600

    modify: vistas de login y register, traducidas al español

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,20 +11,36 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
+        {{-- Styles Gogocar --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <a href="/" class="dark:text-gray-200"">
+                    <div class="text-center">
+                        <i class="fa-solid fa-car-side"></i>
+                        <p>GoGoCar</p>
+                    </div>
                 </a>
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
+            @if (Route::currentRouteName() == 'login')  
+            <div class="mt-2 text-center">
+                <p class="text-xs text-white my-2">¿Aún no estás registrado?</p>
+                <a href="/register">
+                    <button type="button" 
+                        class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                            {{ __('Registrarse') }}
+                    </button>
+                </a>
+            </div>
+            @endif
         </div>
     </body>
 </html>
